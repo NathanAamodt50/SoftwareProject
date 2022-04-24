ec17aed8d56163f814a02cc43b57774767069527
NathanAamodt50 <79725211+NathanAamodt50@users.noreply.github.com>
2022-04-23 21:16:12 -0500

    Add files via upload

---
+++ Final Code/JackrabbitPromotionSoftware/SearchCoup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JackrabbitPromotionSoftware
+{
+    public partial class SearchCoup : JackrabbitPromotionSoftware.GUI
+    {
+        public SearchCoup()
+        {
+            InitializeComponent();
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(textBox1.Text == "Nathan Aamodt")
+            {
+                this.Hide();
+                SearchResults SR = new SearchResults();
+                SR.Show();
+            }
+            else if (textBox1.Text == "Platinum")
+            {
+
+            }
+            else
+            {
+                NoRes NR = new NoRes();
+                NR.Show();
+                KeyPressEventHandler.Equals
+
+            }
+        }
+
+
+    }
+}
---
+++ Final Code/JackrabbitPromotionSoftware/SearchResults.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JackrabbitPromotionSoftware
+{
+    public partial class SearchResults : JackrabbitPromotionSoftware.SearchCoup
+    {
+        public SearchResults()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchResults_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SucessfullySent SS = new SucessfullySent();
+            SS.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SucessfullySent SS = new SucessfullySent();
+            SS.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SucessfullySent SS = new SucessfullySent();
+            SS.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SucessfullySent SS = new SucessfullySent();
+            SS.Show();
+        }
+    }
+}
